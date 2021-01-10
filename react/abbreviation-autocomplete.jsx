486ef395531ce1d44bb9ee7e0863170921ecd418
firelemons <shenyang64@gmail.com>
2021-01-10 05:09:37 -0600

    added type checking for props

--- react/abbreviation-autocomplete.jsx
+++ react/abbreviation-autocomplete.jsx
@@ -6,6 +6,32 @@ class AbbreviationAutocomplete extends React.Component {
 
     this.state = {
     };
+
+    if (!props.data) {
+      throw new Error('Missing required prop data')
+    } else if (!(props.data instanceof Array)) {
+      throw new TypeError(`Prop data must be an array. Instead received ${typeof props.data}`)
+    } else {
+      this.state['data'] = props.data
+    }
+
+    if (isNaN(props.limit)) {
+      throw new TypeError(`Prop limit must be a number. Instead received ${typeof props.limit}`)
+    }
+
+    if (isNaN(props.minSearchTextLength)) {
+      throw new TypeError(`Prop minSearchTextLength must be a number. Instead received ${typeof props.minSearchTextLength}`)
+    }
+
+    if (props.placeholder) {
+      if (typeof props.placeholder !== 'string') {
+        throw new TypeError(`Prop placeholder must be a string. Instead received ${typeof props.placeholder}`)
+      }
+    }
+
+    if (typeof props.searchText !== 'string') {
+      throw new TypeError(`Prop searchText must be a string. Instead received ${typeof props.searchText}`)
+    }
   }
 
   render() {
