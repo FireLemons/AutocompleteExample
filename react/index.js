b69fba493d0ea1f1b13c5940f35762e8ec6007bc
firelemons <shenyang64@gmail.com>
2021-01-15 20:50:12 -0600

    added more info on index

--- react/index.js
+++ react/index.js
@@ -1,8 +1,10 @@
 const domContainer = document.getElementById('app');
-var exampleContents = /*#__PURE__*/React.createElement(AbbreviationAutocomplete, {
+var exampleContents = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Example With Airport IATA Codes"), /*#__PURE__*/React.createElement("p", null, /*#__PURE__*/React.createElement("a", {
+  href: "index.jsx"
+}, "Example JSX")), /*#__PURE__*/React.createElement(AbbreviationAutocomplete, {
   data: airportData,
   limit: 15,
   minSearchTextLength: 2,
   placeholder: "Airport Code"
-});
+}));
 ReactDOM.render(exampleContents, domContainer);
--- react/index.jsx
+++ react/index.jsx
@@ -1,5 +1,10 @@
 const domContainer = document.getElementById('app');
 
-var exampleContents = <AbbreviationAutocomplete data={airportData} limit={15} minSearchTextLength={2} placeholder="Airport Code" />
+var exampleContents =
+  <div>
+    <h1>Example With Airport IATA Codes</h1>
+    <p><a href="index.jsx">Example JSX</a></p>
+    <AbbreviationAutocomplete data={airportData} limit={15} minSearchTextLength={2} placeholder="Airport Code" />
+  </div>
 
 ReactDOM.render(exampleContents, domContainer);
