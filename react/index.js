5c8708136af3eead41d01fb61ab4b5927ba572f4
firelemons <shenyang64@gmail.com>
2021-01-16 20:23:31 -0600

    renamed variable

--- react/index.js
+++ react/index.js
@@ -1,5 +1,5 @@
-const domContainer = document.getElementById('app');
-var exampleContents = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Example With Airport IATA Codes"), /*#__PURE__*/React.createElement("p", null, /*#__PURE__*/React.createElement("a", {
+const container = document.getElementById('app');
+var exampleContents = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Example With Airport IATA Codes"), /*#__PURE__*/React.createElement("p", null, "Open the console to view prop function calls"), /*#__PURE__*/React.createElement("p", null, /*#__PURE__*/React.createElement("a", {
   href: "index.jsx"
 }, "Example JSX")), /*#__PURE__*/React.createElement(AbbreviationAutocomplete, {
   data: airportData,
@@ -7,4 +7,4 @@ var exampleContents = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*
   minSearchTextLength: 2,
   placeholder: "Airport Code"
 }));
-ReactDOM.render(exampleContents, domContainer);
+ReactDOM.render(exampleContents, container);
--- react/index.jsx
+++ react/index.jsx
@@ -1,10 +1,11 @@
-const domContainer = document.getElementById('app');
+const container = document.getElementById('app');
 
 var exampleContents =
   <div>
     <h1>Example With Airport IATA Codes</h1>
+    <p>Open the console to view prop function calls</p>
     <p><a href="index.jsx">Example JSX</a></p>
     <AbbreviationAutocomplete data={airportData} limit={15} minSearchTextLength={2} placeholder="Airport Code" />
   </div>
 
-ReactDOM.render(exampleContents, domContainer);
+ReactDOM.render(exampleContents, container);
