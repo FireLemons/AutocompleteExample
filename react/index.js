9197c671f17a519bbe5afece1f19fe1de60361ea
firelemons <shenyang64@gmail.com>
2021-01-18 00:29:34 -0600

    added onSelect to example

--- react/index.js
+++ react/index.js
@@ -1,10 +1,16 @@
 const container = document.getElementById('app');
+
+function exampleOnSelect(selected) {
+  console.log(selected);
+}
+
 var exampleContents = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Example With Airport IATA Codes"), /*#__PURE__*/React.createElement("p", null, "Open the console to view prop function calls"), /*#__PURE__*/React.createElement("p", null, /*#__PURE__*/React.createElement("a", {
   href: "index.jsx"
 }, "Example JSX")), /*#__PURE__*/React.createElement(AbbreviationAutocomplete, {
   data: airportData,
   limit: 15,
   minSearchTextLength: 2,
-  placeholder: "Airport Code"
+  placeholder: "Airport Code",
+  onSelect: exampleOnSelect
 }));
 ReactDOM.render(exampleContents, container);
--- react/index.jsx
+++ react/index.jsx
@@ -1,11 +1,15 @@
 const container = document.getElementById('app');
 
+function exampleOnSelect(selected) {
+  console.log(selected)
+}
+
 var exampleContents =
   <div>
     <h1>Example With Airport IATA Codes</h1>
     <p>Open the console to view prop function calls</p>
     <p><a href="index.jsx">Example JSX</a></p>
-    <AbbreviationAutocomplete data={airportData} limit={15} minSearchTextLength={2} placeholder="Airport Code" />
+    <AbbreviationAutocomplete data={airportData} limit={15} minSearchTextLength={2} placeholder="Airport Code" onSelect={exampleOnSelect} />
   </div>
 
 ReactDOM.render(exampleContents, container);
