7df7970e3beb3c9c273f5d3e7747ff646b6df01d
firelemons <shenyang64@gmail.com>
2021-01-19 20:19:29 -0600

    added comments and onsearchtextchange

--- react/index.jsx
+++ react/index.jsx
@@ -1,15 +1,43 @@
-const container = document.getElementById('app');
-
 function exampleOnSelect(selected) {
   console.log(selected)
+}
+
+function exampleOnSearchTextChange (newText) {
+  console.log(newText)
 }
 
 var exampleContents =
   <div>
     <h1>Example With Airport IATA Codes</h1>
     <p>Open the console to view prop function calls</p>
-    <p><a href="index.jsx">Example JSX</a></p>
-    <AbbreviationAutocomplete data={airportData} limit={15} minSearchTextLength={2} placeholder="Airport Code" onSelect={exampleOnSelect} />
+    <AbbreviationAutocomplete data={airportData} limit={15} minSearchTextLength={2} placeholder="Airport Code" onSelect={exampleOnSelect} onSearchTextChange={exampleOnSearchTextChange} />
+    {/* data={airportData} */}
+    {/* The autocomplete suggestion data */}
+
+    {/* limit={15} */}
+    {/* Limit the autocomplete dropdown items to a maximum of 15 */}
+
+    {/* minSearchTextlength={2} */}
+    {/* The minimum number of characters to type before dropdown results are shown */}
+
+    {/* placeholder="Airport Code" */}
+    {/* Set the placeholder of the search text input to "Airport Code" */}
+
+    {/* onSelect={exampleOnSelect} */}
+    {/* Call exampleOnSelect when a dropdown item is selected */}
+
+    {/* onSearchTextChange={exampleOnSearchTextChange} */}
+    {/* Call exampleOnSearchTextChange when the search text is changed */}
+    {/* Is not called after the search text is changed by a dropdown item select */}
+
+    {/* The HTML below is not relevant to the example */}
+    <div id="jsx-display">
+      <h2>Contents of index.jsx</h2>
+      <pre>
+        <code>
+        </code>
+      </pre>
+    </div>
   </div>
 
-ReactDOM.render(exampleContents, container);
+ReactDOM.render(exampleContents, document.getElementById('app'));
