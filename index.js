dc72803843794216f6ce2a46df7d051958e38808
firelemons <shenyang64@gmail.com>
2021-01-19 20:03:37 -0600

    simplified html

--- index.js
+++ index.js
@@ -2,10 +2,5 @@ const autocomplete = new Vue({
   el: '#app',
   data: {
     abbreviations: country_data
-  },
-  methods: {
-    onSearchTextChanged(newSearchText) {
-      console.log('input changed to ' + newSearchText)
-    }
   }
 })
